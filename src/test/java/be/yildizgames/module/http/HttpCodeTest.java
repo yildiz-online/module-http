b88cad548eb8a0810eb4fb5c49e580e016359bc9
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-05-07 16:39:46 +0200

    [FIX] Unit tests.

---
+++ src/test/java/be/yildizgames/module/http/HttpCodeTest.java
@@ -0,0 +1,101 @@
+/*
+ * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ *
+ *  Copyright (c) 2019 Grégory Van den Borre
+ *
+ *  More infos available: https://engine.yildiz-games.be
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ *
+ */
+package be.yildizgames.module.http;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Grégory Van den Borre
+ */
+public class HttpCodeTest {
+
+    @Nested
+    public static class IsError {
+
+        @Test
+        public void happyFlow() {
+            for(int i = 400; i < 600; i++) {
+                Assertions.assertTrue(HttpCode.isError(i));
+            }
+        }
+
+        @Test
+        public void tooLow() {
+            Assertions.assertFalse(HttpCode.isError(399));
+        }
+
+        @Test
+        public void tooHigh() {
+            Assertions.assertFalse(HttpCode.isError(600));
+        }
+
+    }
+
+    @Nested
+    public static class IsSuccessful {
+
+        @Test
+        public void happyFlow() {
+            for(int i = 200; i < 300; i++) {
+                Assertions.assertTrue(HttpCode.isSuccessful(i));
+            }
+        }
+
+        @Test
+        public void tooLow() {
+            Assertions.assertFalse(HttpCode.isSuccessful(199));
+        }
+
+        @Test
+        public void tooHigh() {
+            Assertions.assertFalse(HttpCode.isSuccessful(300));
+        }
+
+    }
+
+    @Nested
+    public static class IsRedirected {
+
+        @Test
+        public void happyFlow() {
+            for(int i = 300; i < 400; i++) {
+                Assertions.assertTrue(HttpCode.isRedirected(i));
+            }
+        }
+
+        @Test
+        public void tooLow() {
+            Assertions.assertFalse(HttpCode.isRedirected(299));
+        }
+
+        @Test
+        public void tooHigh() {
+            Assertions.assertFalse(HttpCode.isRedirected(400));
+        }
+
+    }
+
+}
