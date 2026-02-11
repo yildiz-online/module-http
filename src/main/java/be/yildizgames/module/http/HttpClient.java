956b715c5cd5a71ae0964bc642a1fb123895e7b7
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2026-02-11 15:03:07 +0100

    [WIP] New API.

--- src/main/java/be/yildizgames/module/http/HttpClient.java
+++ src/main/java/be/yildizgames/module/http/HttpClient.java
@@ -23,7 +23,7 @@ import java.util.function.Supplier;
 
 /**
  * Http call to retrieve text or binary content.
- * It can either provide simple call or call with a behavior to be notified when the call is done.
+ * Every call will return an http response that can either be handled or simply provide an optional body.
  *
  * @author Grégory Van den Borre
  */
@@ -33,34 +33,70 @@ public interface HttpClient {
      * Request the text content.
      *
      * @param to URI to reach, must be a valid URI, cannot be null.
-     * @throws IllegalStateException in case of failure, being technical or by receiving a 4xx or 5xx http code.
-     * @return The body of the response to the call to the given URI, can be empty, never null.
+     * @return The response to the call to the given URI, the body is a String, it can be empty, never null.
      */
     @API(status = API.Status.STABLE)
     HttpResponse<String> getText(final String to);
 
-    HttpResponse<String> postText(final String to, final String textToPost, String mime);
+    /**
+     * Send a text content and receive a text response.
+     *
+     * @param to      Destination.
+     * @param content Text to send.
+     * @param mime    Data mime type.
+     * @return A text response, it can be empty.
+     */
+    HttpResponse<String> postText(final String to, final String content, String mime);
 
+    /**
+     * Request a binary content and persist it as a file.
+     *
+     * @param to               URI to reach, must be a valid URI, cannot be null.
+     * @param destination      Path to the file to create, cannot be null.
+     * @param transferListener Listener to track the download progress, can be null.
+     * @return The response to the call to the given URI, the body is the provided path for the file to save, it can be empty, never null.
+     */
     @API(status = API.Status.STABLE)
-    HttpResponse<Path> getFile(String to, Path destination, HttpTransferListener l);
+    HttpResponse<Path> getFile(String to, Path destination, HttpTransferListener transferListener);
 
+    /**
+     * Request a binary content and persist it as a file.
+     *
+     * @param to          URI to reach, must be a valid URI, cannot be null.
+     * @param destination Path to the file to create, cannot be null.
+     * @return The response to the call to the given URI, the body is the provided path for the file to save, it can be empty, never null.
+     */
     @API(status = API.Status.STABLE)
     HttpResponse<Path> getFile(String to, Path destination);
 
     /**
-     * Send a file and receive a response.
+     * Send a file and receive a text response.
      *
-     * @param to   Destination.
-     * @param file File to send.
-     * @param mime File mime type.
-     * @return A text response.
+     * @param to      Destination.
+     * @param content File to send.
+     * @param mime    File mime type.
+     * @return A text response, it can be empty.
      */
     @API(status = API.Status.STABLE)
-    HttpResponse<String> postFile(String to, Path file, String mime);
+    HttpResponse<String> postFile(String to, Path content, String mime);
 
+    /**
+     * Request a binary content as InputStream.
+     *
+     * @param to URI to reach, must be a valid URI, cannot be null.
+     * @return The response to the call to the given URI, the body is the InputStream to retrieve the binary content, never null.
+     */
     @API(status = API.Status.STABLE)
     HttpResponse<InputStream> getInputStream(final String to);
 
+    /**
+     * Send a binary content as InputStream and receive a text response.
+     *
+     * @param to      Destination.
+     * @param content Data to send.
+     * @param mime    Data mime type.
+     * @return A text response, it can be empty.
+     */
     @API(status = API.Status.STABLE)
     HttpResponse<String> postInputStream(String to, Supplier<InputStream> content, String mime);
 
