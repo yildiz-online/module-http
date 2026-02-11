2aef398065fa1d3f10b5f7c4f643d799e37f1183
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2026-02-11 12:35:35 +0100

    [WIP] New API.

--- src/main/java/be/yildizgames/module/http/HttpClient.java
+++ src/main/java/be/yildizgames/module/http/HttpClient.java
@@ -18,9 +18,8 @@ package be.yildizgames.module.http;
 import org.apiguardian.api.API;
 
 import java.io.InputStream;
-import java.io.Reader;
-import java.net.URI;
 import java.nio.file.Path;
+import java.util.function.Supplier;
 
 /**
  * Http call to retrieve text or binary content.
@@ -33,59 +32,17 @@ public interface HttpClient {
     /**
      * Request the text content.
      *
-     * @param to URI to reach, cannot be null.
-     * @throws IllegalStateException in case of failure, being technical or by receiving a 4xx or 5xx http code.
-     * @return The body of the response to the call to the given URI, can be empty, never null.
-     */
-    @API(status = API.Status.STABLE)
-    String getText(final URI to);
-
-    /**
-     * Request the text content.
-     *
      * @param to URI to reach, must be a valid URI, cannot be null.
      * @throws IllegalStateException in case of failure, being technical or by receiving a 4xx or 5xx http code.
      * @return The body of the response to the call to the given URI, can be empty, never null.
      */
     @API(status = API.Status.STABLE)
-    String getText(final String to);
+    HttpResponse<String> getText(final String to);
+
+    HttpResponse<String> postText(final String to, final String textToPost, String mime);
 
     @API(status = API.Status.STABLE)
-    HttpResponse<String> getTextResponse(final URI uri);
-
-    @API(status = API.Status.STABLE)
-    HttpResponse<String> getTextResponse(final String uri);
-
-    <T,R> HttpResponse<R> postObject(final String uri, T objectToPost, Class<R> responseClazz);
-
-    /**
-     * Make a request expecting a json object, and return
-     *
-     * @param to    Address to call.
-     * @param clazz Class of the object to return.
-     * @param <T>   Type of the object to return.
-     * @return The mapped object.
-     */
-    @API(status = API.Status.STABLE)
-    <T> T getObject(URI to, Class<T> clazz);
-
-    @API(status = API.Status.STABLE)
-    <T> T getObject(String to, Class<T> clazz);
-
-    @API(status = API.Status.STABLE)
-    InputStream getInputStream(final URI to);
-
-    @API(status = API.Status.STABLE)
-    InputStream getInputStream(final String to);
-
-    @API(status = API.Status.STABLE)
-    Reader getReader(final URI to);
-
-    @API(status = API.Status.STABLE)
-    Reader getReader(final String to);
-
-    @API(status = API.Status.STABLE)
-    void sendFile(URI to, Path file, String mime);
+    HttpResponse<Path> getFile(String to, Path destination, HttpTransferListener l);
 
     /**
      * Send a file and receive a response.
@@ -96,14 +53,13 @@ public interface HttpClient {
      * @return A text response.
      */
     @API(status = API.Status.STABLE)
-    HttpResponse<String> sendFileResponse(URI to, Path file, String mime);
+    HttpResponse<String> postFile(String to, Path file, String mime);
 
     @API(status = API.Status.STABLE)
-    HttpResponse<String> sendBinaryResponse(URI to, byte[] content, String mime);
+    HttpResponse<InputStream> getInputStream(final String to);
 
     @API(status = API.Status.STABLE)
-    void receiveFile(URI to, Path destination);
+    HttpResponse<String> postInputStream(String to, Supplier<InputStream> content, String mime);
 
-    @API(status = API.Status.STABLE)
-    void addTransferListener(HttpTransferListener l);
+
 }
